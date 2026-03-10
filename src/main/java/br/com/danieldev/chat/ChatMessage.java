c59cca379fd24c246a7f10ef2fcc5a1f65ed5ac3
Daniel Santos Oliveira <danlox0602@gmail.com>
2026-03-10 09:12:12 -0300

    Update ChatMessage.java

--- src/main/java/br/com/danieldev/chat/ChatMessage.java
+++ src/main/java/br/com/danieldev/chat/ChatMessage.java
@@ -1,49 +1,24 @@
 package br.com.danieldev.chat;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
     private String from;
     private String content;
-    private String type;
+    private MessageType type;
     private LocalDateTime timestamp;
 
-    public ChatMessage(String from, String content, String type, LocalDateTime timestamp) {
-        this.from = from;
-        this.content = content;
-        this.type = type;
-        this.timestamp = timestamp;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
     }
 }
